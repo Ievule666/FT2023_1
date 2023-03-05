83cdc793a6feed1d7c627e5020df3eed332604f6
Ievule666 <101431267+Ievule666@users.noreply.github.com>
2023-03-05 23:44:49 +0200

    Pridejau changeText mygtuka ir implementavau logika

--- app/src/main/java/com/example/ft2023_1/MainActivity.java
+++ app/src/main/java/com/example/ft2023_1/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.ft2023_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btnChangeMainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = findViewById(R.id.textView);
+        this.btnChangeMainText = (Button) findViewById(R.id.btnChangeMainText);
+
+        btnChangeMainText.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+                   textView.setText("Sveiki kolegos");
+            }
+        });
+
     }
 }
